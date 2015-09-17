cec593b2acbf28b9798446f46f09270af986c6c0
Alec Tunbridge <alec.tunbridge@gmail.com>
2015-09-17 18:17:56 +0100

    Actually we weren't far off....

--- src/main/java/BinaryChop.java
+++ src/main/java/BinaryChop.java
@@ -3,13 +3,14 @@ import java.util.Collection;
 
 public class BinaryChop {
     public int chop(int searchingFor, int[] data) {
+        int result;
+        int size = data.length;
+        int middle = size/2;
+        int offset = 0;
 
         if(data.length == 0) {
             return -1;
         }
-
-        int size = data.length;
-        int middle = size/2;
 
         if(data[middle] == searchingFor) {
             return middle;
@@ -19,21 +20,17 @@ public class BinaryChop {
             return -1;
         }
 
-        int result = -1;
         if(searchingFor > data[middle]) {
             result = chop(searchingFor, Arrays.copyOfRange(data, middle, data.length));
+            offset = middle;
         } else {
             result = chop(searchingFor, Arrays.copyOfRange(data, 0, middle));
         }
 
         if ( result != -1 ) {
-            return middle + result;
+            return offset + result;
         }
-        return -1;
 
-    }
-
-    private boolean thisIsNotTheNumberYoureLookingFor(int searchingFor, int[] data) {
-        return data.length == 1 & data[0] != searchingFor;
+        return result;
     }
 }
