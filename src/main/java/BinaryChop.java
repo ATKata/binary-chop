27085e8e30efe4e9ec9396825c49f984c7024621
Alec Tunbridge <alec.tunbridge@gmail.com>
2015-09-17 09:48:31 +0100

    Initial commit

---
+++ src/main/java/BinaryChop.java
@@ -0,0 +1,7 @@
+import java.util.Collection;
+
+public class BinaryChop {
+    public int chop(int searchingFor, int[] data) {
+        throw new IllegalArgumentException("Not yet implemented!");
+    }
+}
